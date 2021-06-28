b15095e386ab3c9aa6ef027f105109f0707a5fa7
Wu Peirong <wupeirong294@gmail.com>
2021-06-28 18:34:53 +0800

    Remove dividend command

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { Command } from 'commander';
 import { init } from './commands/init';
 import { start } from './commands/start';
 import { find } from './commands/find';
-import { div } from './commands/div';
 
 const program = new Command();
 
@@ -12,8 +11,6 @@ program.version('0.0.1');
 program.command('init').description('initialises the portfolio config').action(init);
 
 program.command('start').description('starts processing orders in portfolio config').action(start);
-
-program.command('div').description('gets dividends').action(div);
 
 program
   .command('find')
