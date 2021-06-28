727c0432160be80f3d72c430583e0ba9f3965924
Wu Peirong <wupeirong294@gmail.com>
2021-06-28 14:35:17 +0800

    Change local config path

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,9 @@
 import path from 'path';
+import { homedir } from 'os';
 import { QuoteField } from 'yahoo-finance2/dist/esm/src/modules/quote';
 import { ComputedProperties, PortfolioConfig } from './types';
 
-export const LOCAL_CONFIG_PATH = path.join(__dirname, '../..', '.summariser.json');
+export const LOCAL_CONFIG_PATH = path.join(homedir(), '.summariser.json');
 
 export const DEFAULT_CONFIG: PortfolioConfig = {
   orders: [{ ticker: 'AAPL', purchaseDate: '2019-5-3', volume: 1000, cost: 124 }],
